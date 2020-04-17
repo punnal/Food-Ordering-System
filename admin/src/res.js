3fe6ac2aa073dabcf88e522b0a917a2aa9d7b7f8
AhmadHumayun <ahmadhumayun3.5@gmail.com>
2020-04-18 01:16:53 +0500

    Separated Components into separate files, using res.js to construct navbar, added classnames to res.js

---
+++ admin/src/res.js
@@ -0,0 +1,31 @@
+export const res = {
+    'admin': {
+        'navbar': {
+            'deliveries':'Deliveries',
+            'order':'Order',
+            'menu':'Menu',
+            'deals':'Deals',
+            'gallery':'Gallery',
+            'aboutus':'About Us',
+            'contactus':'Contact Us',
+            'divider':'', //this marks the position of the divider... do not change spellings
+            'accountdropdown':{
+                'title':'Hi, Admin',
+                'options': ['History', 'Settings', 'Sign Out']
+            }
+        },
+
+        'css_classes': {
+            'NavBar': '',  //the <div> that wraps NavBarElements
+            'NavBarElement': '',  // <div>
+            'NavBarDropdown': '',  // <div>
+            'Footer': '', //div
+            'Header': '', //div
+            'Body': '', //div
+            'NavBarDivider': '' //div
+        }
+
+    }
+
+
+}
