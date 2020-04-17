34f26c6df24b0d5889f3eb9d54c7868ce0ee376b
punnal <21100019@lums.edu.pk>
2020-04-18 01:45:24 +0500

    Added Dummy NavigationBar, MainContents, Footer components to App.js

---
+++ customer/src/Components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <h1>This is NavigationBar</h1>
+        </div>
+    )
+}
+
+export default NavigationBar
