daa0889673b9b5a50cad580fa21f525971210d62
Ayaz <2110036.lums.edu.pk>
2020-04-27 20:42:59 +0500

    merged with customer

--- customer/src/Components/MainContents.js
+++ customer/src/Components/MainContents.js
@@ -13,39 +13,45 @@ import Orders from "./Pages/Orders";
 import Classes from "../Resource/className";
 
 const MainContents = (props) => {
-    return (
-        <div className={Classes.MainContents}>
-            <Switch>
-                <Route exact path="/">
-                    <Menu orders={props.orders} addOrders={props.addOrders}/>
-                </Route>
-                <Route path="/deals">
-                    <Deals orders={props.orders} addOrders={props.addOrders}/>
-                </Route>
-                <Route path="/gallery">
-                    <Gallery />
-                </Route>
-                <Route exact path="/about">
-                    <AboutUs />
-                </Route>
-                <Route path="/contact">
-                    <ContactUs />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route exact path="/signup">
-                    <SignUp />
-                </Route>
-                <Route path="/cart">
-                    <Cart orders={props.orders} addOrders={props.addOrders} deleteOrder={props.deleteOrder} changeQuantity={props.changeQuantity} resetOrders={props.resetOrders}/>
-                </Route>
-                <Route path="/orders">
-                    <Orders />
-                </Route>
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div className={Classes.MainContents}>
+      <Switch>
+        <Route exact path="/">
+          <Menu orders={props.orders} addOrders={props.addOrders} />
+        </Route>
+        <Route path="/deals">
+          <Deals orders={props.orders} addOrders={props.addOrders} />
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+        <Route exact path="/about">
+          <AboutUs />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/cart">
+          <Cart
+            orders={props.orders}
+            addOrders={props.addOrders}
+            deleteOrder={props.deleteOrder}
+            changeQuantity={props.changeQuantity}
+            resetOrders={props.resetOrders}
+          />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default MainContents;
