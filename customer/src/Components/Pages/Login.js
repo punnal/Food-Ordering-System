7edf4960e5cab2263e8a5c18ad00cc61d7acd418
StrawHat-14 <smhmz51214@yahoo.com>
2020-04-28 21:16:59 +0500

    added Sign up and login without the functionality

--- customer/src/Components/Pages/Login.js
+++ customer/src/Components/Pages/Login.js
@@ -12,8 +12,30 @@ class Login extends React.Component {
 
     render() {
         return(
-            <div>
-                This is a template.
+            <div id = "Login" className  = "container">
+                <h2 className = "mt-5">User Account</h2>
+                <form class="needs-validation"  action = "/action_page.php" novalidate>
+                    <div className = "form-group">
+                        <label for = "email">Email:</label>
+                        <input type = "email" className = "form-control" id = "email" placeholder = "Enter email" name = "email" required />
+                        <div class="valid-feedback">Good to go!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div className = "form-group">
+                        <label for = "pass">Password:</label>
+                        <input type = "password" className = "form-control" id = "pass" placeholder = "Enter password" name = "passwords" required />
+                        <div class="valid-feedback">All done!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div className = "custom-control custom-checkbox mb-3">
+                        <input type="checkbox" className = "custom-control-input" id="customCheck" name="remember" />
+                        <label className = "custom-control-label" for="customCheck">Remember Me</label>
+                    </div>
+                    <button type="submit" className = "btn btn-dark">Login</button>
+                </form>
+                <h3>Or</h3>
+                <img className = "mx-auto d-block" src = {require("../../img/google.png")} height = '50' weight = '50' />
+                <p>Forgot password?</p>
             </div>
         )
     }
--- customer/src/Components/Pages/SignUp.js
+++ customer/src/Components/Pages/SignUp.js
@@ -12,8 +12,53 @@ class SignUp extends React.Component {
 
     render() {
         return(
-            <div>
-                This is a template.
+            <div id = "SignUp" className  = "container">
+                <h2 className = "mt-5">User Account</h2>
+                <form class="needs-validation"  action = "/action_page.php" novalidate>
+                    <div className = "form-group">
+                        <div className = "row">
+                            <div className = "col">
+                                <input type="text" className = "form-control" id="firstName" placeholder="Enter First Name" name="FirstName" required />
+                            </div>
+                            <div className="col">
+                                <input type="text" className="form-control" placeholder="Enter Last Name" name="LastName" required />
+                            </div>
+                        </div>
+                    </div>
+                    <div className = "form-group">
+                        <input type = "email" className = "form-control" id = "email" placeholder = "Enter email" name = "email" required />
+                        <div class="valid-feedback">Good to go!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div className = "form-group">
+                        <input type = "password" className = "form-control" id = "pass" placeholder = "Enter password" name = "passwords" min = "8" pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required />
+                        <div class="valid-feedback">All done!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div className = "form-group">
+                        <input type = "password" className = "form-control" id = "pass" placeholder = "Re-enter Password" pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" name = "passwords" required />
+                        <div class="valid-feedback">All done!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <p>The password provided must be at least 8 characters long, containing at least one digit, one capital letter, one small letter.</p>
+                    <h2 className = "mt-5">Contact Information</h2>
+                    <div className = "form-group">
+                        <input type = "text" className = "form-control" id = "address" placeholder = "Address" name = "Address" required />
+                        <div class="valid-feedback">Good to go!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <div className = "form-group">
+                        <input type = "tel" className = "form-control" id = "phone" placeholder = "Phone Number" name = "Phone" pattern = "[0-9]{4}-[0-9]{7}" required />
+                        <div class="valid-feedback">Good to go!</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                    </div>
+                    <p>Phone number should be entered in this format: xxxx-xxxxxxx</p>
+                    <div className = "custom-control custom-checkbox mb-3">
+                        <input type="checkbox" className = "custom-control-input" id="customCheck" name="remember" />
+                        <label className = "custom-control-label" for="customCheck">Remember Me</label>
+                    </div>
+                    <button type="submit" className = "btn btn-dark">SignUp</button>
+                </form>
             </div>
         )
     }
