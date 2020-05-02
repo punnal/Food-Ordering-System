cfa50429406b14d21a5ed3ca4e871b82817e9e61
punnal <21100019@lums.edu.pk>
2020-05-02 19:41:58 +0500

    About us page now recieved data from server

--- customer/src/Components/Pages/AboutUs.js
+++ customer/src/Components/Pages/AboutUs.js
@@ -1,25 +1,52 @@
 import React from 'react'
+import Axios from 'axios'
 
+import Api from '../../api/api'
 
 class AboutUs extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            
+            contents: {
+                heading: "",
+                description: "",
+            } 
         }
     }
 
+    componentWillMount(){
+        this.setState({loading: true}, () =>
+                Axios.get(Api.about)
+                    .then((response) => {
+                        console.log(response.data)
+                        console.log(response.data.data)
+                        this.setState({
+                            contents:response.data
+                        }, () => this.setState({
+                            loading: false
+                        }))
+                    }).catch(() => {
+                        this.setState({
+                            //Hardcoded here
+                            contents:{
+                                heading:"About Us",
+                                description:"This is the Best place to eat"
+                            }, 
+                        }, () => this.setState({
+                            loading: false
+                        }))
+                    })
+        )
+
+    }
     render() {
         return(
             <div className = "AboutUsContainer">
                 <div id = "AboutUsFirst">
                     <div className = "AboutUsInfo">
-                        <h1 className = "AboutUsSubHeadingLeft">About Us</h1>
-                        <p>Quay is one of the country’s most celebrated restaurants; the creation of leading Australian restaurant group, Fink, and Executive Chef Peter Gilmore.
-                            The restaurant is an organic space reflective of Peter Gilmore’s nature inspired cuisine. The interplay of textures and colour brings life and a
-                            vibrance that embraces the restaurant’s place in the dress circle of Sydney Harbour. An ode to the Australian landscape, from the vast ocean floor,
-                            to the cracked bark of a paperbark tree, every detail from the ground up has been thoughtfully considered.</p>
+                        <h1 className = "AboutUsSubHeadingLeft">{this.state.contents.heading}</h1>
+                        <p>{this.state.contents.description}</p>
                     </div>
                     <img className = "AboutUsImageRight" src = "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" width = "765" alt = "About Us"/>
                 </div>
