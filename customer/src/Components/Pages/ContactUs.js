92b95251f13f0acc16cf4cf379afae911deb0c72
Ayaz <2110036.lums.edu.pk>
2020-05-06 00:56:43 +0500

    positioning

--- customer/src/Components/Pages/ContactUs.js
+++ customer/src/Components/Pages/ContactUs.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import * as emailjs from "emailjs-com";
 import Maps from "./GoogleMaps";
 import Loc from "../../img/location.png";
+import Iframe from "react-iframe";
 
 export default class Contact extends React.Component {
   state = {
@@ -161,33 +162,38 @@ export default class Contact extends React.Component {
 
   render() {
     return (
-      <div className = "ContactUsMain">
+      <div className="ContactUsMain">
         <div>
           <div class="container text-white pt-3" id="contactForm">
-            <h2 className = "ContactUsTitle">Have some suggestions?</h2>
-            <h4 className = "ContactUsSubTitle">Send us your feedback</h4>
+            <h2 className="ContactUsTitle">Have some suggestions?</h2>
+            <h4 className="ContactUsSubTitle">Send us your feedback</h4>
             <div className="full-width-row cta-form--bg">
               <div className="row-container">
                 <div class="needs-validation">
-                  <input className = "form-control"
+                  <input
+                    className="form-control"
                     placeholder="Enter Name"
                     name="name"
                     value={this.state.name}
                     onChange={(e) => this.change(e)}
                     type="text"
                     id="name"
-                    required/>
+                    required
+                  />
                   <p className="form-error">{this.state.nameError}</p>
-                  <input className = "form-control"
+                  <input
+                    className="form-control"
                     placeholder="Enter Email"
                     name="email"
                     value={this.state.email}
                     onChange={(e) => this.change(e)}
                     type="text"
                     id="email"
-                    required/>
+                    required
+                  />
                   <p className="form-error">{this.state.emailError}</p>
-                  <input className = "form-control"
+                  <input
+                    className="form-control"
                     placeholder="Subject"
                     name="subject"
                     value={this.state.subject}
@@ -197,7 +203,8 @@ export default class Contact extends React.Component {
                     required
                   />
                   <p className="form-error">{this.state.subjectError}</p>
-                  <textarea className = "form-control"
+                  <textarea
+                    className="form-control"
                     placeholder="Your feedback message"
                     name="message"
                     onChange={(e) => this.change(e)}
@@ -206,39 +213,71 @@ export default class Contact extends React.Component {
                     required
                   />
                   <p className="form-error">{this.state.messageError}</p>
-                  <button id = "ContactUsSubmit" class = "btn btn-dark" type="button"onClick={(e) => this.onSubmit(e)}>Send</button>
+                  <button
+                    id="ContactUsSubmit"
+                    class="btn btn-dark"
+                    type="button"
+                    onClick={(e) => this.onSubmit(e)}
+                  >
+                    Send
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div id = "ContactUsMessageBox" class="container bg-dark text-white pt-3">
-          <div id = "ContactUsContacts" className="footer-section contact-form">
-              <h3>Contact</h3>
-              <div className = "ContactUsIcons">
-                <a href="https://www.facebook.com/smoke.and.grill.modeltown.lahore"><i className = "fab fa-facebook"></i></a>
-                <a href="#"><i className = "fab fa-twitter"></i></a>
-                <a href="#"><i className = "fab fa-instagram"></i></a>
-                <a href="#"><i className = "fab fa-linkedin"></i></a>
-                <a href="#"><i className = "fab fa-youtube"></i></a>
-              </div>
-              <div>
-                <i id = "ContactUsPhone" className = "fa fa-phone"></i>
-                <span>+92 311 7210000</span>
-              </div>
-              <div>
-                <i id = "ContactUsPhone" class="fa fa-envelope"></i>
-                <span><a className ="ContactUsMail" href = "mailto:smokeandgrill123@gmail.com">smokeandgrill123@gmail.com</a></span>
-              </div>
-                <Maps />
+        <div id="ContactUsMessageBox" class="container bg-dark text-white pt-3">
+          <div id="ContactUsContacts" className="footer-section contact-form">
+            <h3>Contact</h3>
+            <div className="ContactUsIcons">
+              <a href="https://www.facebook.com/smoke.and.grill.modeltown.lahore">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-youtube"></i>
+              </a>
+            </div>
+            <div>
+              <i id="ContactUsPhone" className="fa fa-phone"></i>
+              <span>+92 311 7210000</span>
+            </div>
+            <div>
+              <i id="ContactUsPhone" class="fa fa-envelope"></i>
+              <span>
+                <a
+                  className="ContactUsMail"
+                  href="mailto:smokeandgrill123@gmail.com"
+                >
+                  smokeandgrill123@gmail.com
+                </a>
+              </span>
+            </div>
+            <div style={{ width: "100%", height: "500px" }}>
+              <Iframe
+                url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3402.5806055763965!2d74.30882415113054!3d31.480721156156484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391903ef7eaa6e75%3A0x12c224a40a628cf1!2sSmoke%20%26%20Grill!5e0!3m2!1sen!2s!4v1588707825977!5m2!1sen!2s"
+                width="100%"
+                height="100%"
+                id="myId"
+                className="myClassname"
+                display="initial"
+                position="relative"
+              />
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
-
-
 
 /*style={{
                       width: "400px",
