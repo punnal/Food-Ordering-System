37dc220fb20d1d93e2da17ed71ec9e547739a113
AhmadHumayun <ahmadhumayun3.5@gmail.com>
2020-05-06 21:19:07 +0500

    made login

--- admin/src/Components/Login.js
+++ admin/src/Components/Login.js
@@ -1,9 +1,57 @@
 import React from "react"
+import {api_push, api_pull} from '../api/api'
 
 class Login extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.api = '/admin/api/login'
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onLogin = this.onLogin.bind(this)
+    }
+
+    onChange(event) {
+        const {id, value} = event.target
+        this.setState(old => {
+            return {
+                ...old,
+                [id]:value
+            }
+        })
+    }
+
+    onLogin(){
+        api_push(this.api, {...this.state})
+        this.props.setAuth()
+    }
+
     render() {
-        return <div> Placeholder </div>
+        return (
+            <div>
+                <input 
+                    value={this.state.username} 
+                    onChange={this.onChange}
+                    type="text" 
+                    id="username"
+                />
+                <input 
+                    value={this.state.password} 
+                    onChange={this.onChange}
+                    type="password" 
+                    id="password"
+                />
+                <button 
+                    class="btn btn-success"
+                    onClick={this.onLogin}
+                >
+                    Login
+                </button>
+            </div>
+        )
     }
 }
 
